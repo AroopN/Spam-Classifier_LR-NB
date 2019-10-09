d700b33fe04c9a2cf3114af95598cd9aec94aefc
aroop <aroopnanda@gmail.com>
2019-10-09 08:53:37 -0500

    added stopwords exclusion flag

--- Classfier.py
+++ Classfier.py
@@ -2,27 +2,9 @@ from read_data import *
 from math import log
 
 if __name__ == '__main__':
-    spams,spam_words, spam_word_counts = read_data("./train/spam/")
-    hams, ham_words, ham_word_counts = read_data("./train/ham/")
-    spam_prior = spams/(spams+hams)
-    ham_prior = hams/(spams+hams)
-    all_words = get_allwords()
-    all_words = remove_stopwords(all_words)
-    spam_word_counts = laplace_smoothening(all_words, spam_word_counts, 1)
-    ham_word_counts = laplace_smoothening(all_words, ham_word_counts, 1)
-    # print(ham_words[99])
-
-    tot_spam_words = sum(spam_word_counts.values())
-    spam_word_prob = spam_word_counts
-    for i in all_words:
-        spam_word_prob[i] = spam_word_prob[i]/tot_spam_words
-
-    tot_ham_words = sum(ham_word_counts.values())
-    ham_word_prob = ham_word_counts
-    for i in all_words:
-        ham_word_prob[i] = ham_word_prob[i]/tot_ham_words
-
-
+    rem_stopwords=0
+    spam_prior, ham_prior, spam_word_prob, ham_word_prob = trainNB(
+        rem_stopwords)
     mypath = "./test/spam/"
     f = fnames(mypath)
     X = []
@@ -30,7 +12,10 @@ if __name__ == '__main__':
     for i in f:
         fin = codecs.open(mypath+i, 'r', encoding='utf-8',
                         errors='ignore')
-        X.append(remove_stopwords(preprocess(fin.read()).split()))
+        sen = preprocess(fin.read()).split()
+        if rem_stopwords == 1:
+            sen = remove_stopwords(sen)
+        X.append(sen)
         y.append(1)
 
 
@@ -41,12 +26,15 @@ if __name__ == '__main__':
     for i in f:
         fin = codecs.open(mypath+i, 'r', encoding='utf-8',
                         errors='ignore')
-        sen = remove_stopwords(preprocess(fin.read()).split())
+
+        sen = preprocess(fin.read()).split()
+        if rem_stopwords==1:
+            sen=remove_stopwords(sen)
         X.append(sen)
         y.append(0)
 
     # print(X[10])
-    print(spam_prior)
+    # print(spam_prior)
     pred = []
     for i in X:
         sp = log(spam_prior)
--- read_data.py
+++ read_data.py
@@ -20,7 +20,7 @@ def remove_stopwords(d):
             new_d.append(i)
     return new_d
 
-def counts(d):
+def counts(d,stopwords=1):
     d = remove_stopwords(d)
     count={}
     for i in d:
@@ -48,7 +48,7 @@ def fnames(mypath):
 def read_data(mypath):
     
     words = []
-    word_count = {}
+    # word_count = {}
     # mypath="./train/spam/"
     f = fnames(mypath)
     data=""
@@ -60,9 +60,9 @@ def read_data(mypath):
         data+=preprocess(fin.read())
         data+=" "
     words = data.split()
-    word_count=counts(data.split())
+    # word_count=counts(data.split())
     # print(spam_words)
-    return prior,words,word_count
+    return prior,words
 
     # print(spam)
 
@@ -74,8 +74,34 @@ def get_allwords():
         for i in f:
             fin = codecs.open(mypath+i, 'r', encoding='utf-8',
                             errors='ignore')
-            sen = remove_stopwords(preprocess(fin.read()).split())
+            sen = preprocess(fin.read()).split()
             allwords.extend(sen)
     return set(allwords)
 
-# print(counts("i am a beg beg student i am not in a nug me please tea".split()))
+def trainNB(rem_stopwords=1):
+    
+    spams, spam_words = read_data("./train/spam/")
+    spam_word_counts = counts(spam_words, rem_stopwords)
+    hams, ham_words = read_data("./train/ham/")
+    ham_word_counts = counts(ham_words, rem_stopwords)
+    spam_prior = spams/(spams+hams)
+    ham_prior = hams/(spams+hams)
+    all_words = get_allwords()
+    if rem_stopwords==1:
+        all_words = remove_stopwords(all_words)
+
+    spam_word_counts = laplace_smoothening(all_words, spam_word_counts, 1)
+    ham_word_counts = laplace_smoothening(all_words, ham_word_counts, 1)
+    # print(ham_words[99])
+
+    tot_spam_words = sum(spam_word_counts.values())
+    spam_word_prob = spam_word_counts
+    for i in all_words:
+        spam_word_prob[i] = spam_word_prob[i]/tot_spam_words
+
+    tot_ham_words = sum(ham_word_counts.values())
+    ham_word_prob = ham_word_counts
+    for i in all_words:
+        ham_word_prob[i] = ham_word_prob[i]/tot_ham_words
+
+    return spam_prior, ham_prior, spam_word_prob, ham_word_prob
